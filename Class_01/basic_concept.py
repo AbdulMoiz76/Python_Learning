72c87dae83b98b36c286c70e94a2220ec5ef8d49
AbdulMoiz76 <mmypak910@gmail.com>
2025-05-16 09:31:55 +0500

    variable

--- Class_01/basic_concept.py
+++ Class_01/basic_concept.py
@@ -1,2 +1,50 @@
-ab = "Abdul Moiz"
-print(ab)😂
+#  Variables
+
+String = "Abdul Moiz"
+number = 3260107654
+float = 20.1
+am_single = True
+
+print(String)
+print(number)
+print(float)
+print(am_single)
+#  Data Types
+# 1. String
+print(type(String))
+# 2. Integer
+print(type(number))
+# 3. Float
+print(type(float))
+# 4. Boolean
+print(type(am_single))
+
+
+
+# pre_define_functions
+# 1. print()
+print("Hello World")
+# 2. type()
+print(type(String))
+# 3. input()
+input_var = input("Enter your name: ")
+print(input_var)
+# 4. len()
+
+# 5. str()
+# 6. int()
+# 7. float()
+# 8. bool()
+# 9. list()
+# 10. dict()
+# 11. set()
+# 12. tuple()
+# 13. range()
+# 14. sum()
+# 15. min()
+# 16. max()
+# 17. sorted()
+# 18. reversed()
+# 19. enumerate()
+# 20. zip()
+# 21. map()
