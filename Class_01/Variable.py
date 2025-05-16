d7b74435652154b61d916b0e4179fca6e4929b5b
AbdulMoiz76 <mmypak910@gmail.com>
2025-05-16 10:27:01 +0500

    add

---
+++ Class_01/Variable.py
@@ -0,0 +1,23 @@
+#  Variables
+
+String = "Abdul Moiz"
+number = 3260107654
+float = 20.1
+am_single = True
+
+print(String)
+print(number)
+print(float)
+print(am_single)
+#  Data Types
+# 1. String
+print(type(String))
+# 2. Integer
+print(type(number))
+# 3. Float
+print(type(float))
+# 4. Boolean
+print(type(am_single))
+
+
+
