7cee17ae660cf022fbc5a0b5f7e4fd4c832ff1d7
AbdulMoiz76 <mmypak910@gmail.com>
2025-05-17 21:57:16 +0500

    add

---
+++ Class_03/for-else.py
@@ -0,0 +1,11 @@
+successful = False
+
+for number in range(3):
+    print("Attempt")
+    if (successful):
+        print("Successful")
+        break
+else:
+     print("Unsuccessful")
+
+
---
+++ Class_03/loop.py
@@ -0,0 +1,9 @@
+for number in range(10 , 0, -1):
+    print("Star" , number * "*")
+
+
+for number in range(1,10):
+    print("Star", number * "*")
+
+for number in range(10,0,-1):
+    print("star",number*"*")
