2f8030ae1969b4c90dd2f1c65d4ccc999359e229
AbdulMoiz76 <mmypak910@gmail.com>
2025-05-18 05:41:58 +0500

    Practice

--- Class_03/for-else.py
+++ Class_03/for-else.py
@@ -9,3 +9,17 @@ else:
      print("Unsuccessful")
 
 
+table = int(input("Enter your number:  "))
+
+for table_num in range (1,11):
+    print(f"{table} * {table_num} = {table * table_num}")
+
+student_name = input("Enter your name: ")
+name = ["Ali", "Ahmed", "Mubeen", "Sulman", "Hassan", "Abdul rehman", "Ali Raza"]
+
+for present in range(1,10):
+    if student_name == name[present-1]:
+        print("Marked attendance")
+        break
+else:
+    print("Attendance not marked Please try again")
