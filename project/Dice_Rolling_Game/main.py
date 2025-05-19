b6817ffe50844a3ec8fb2839e44dcc06525a27fd
AbdulMoiz76 <mmypak910@gmail.com>
2025-05-19 10:41:04 +0500

    Dice Rolling Game

---
+++ project/Dice_Rolling_Game/main.py
@@ -0,0 +1,20 @@
+import random
+
+while True:
+
+    Choice = input(
+        "Do you wnat to play a dice rolling game? (yes/no):  ").lower()
+    if Choice == "yes":
+        print("Welcome to the Dice Rolling Game!")
+        dice_roll_1 = random.randint(1, 6)
+        dice_roll_2 = random.randint(1, 6)
+        print(f"You rolled a {dice_roll_1} and a {dice_roll_2}")
+    elif Choice == "no":
+        print("you have play game just fun! ")
+        continue
+    elif Choice == "quit".lower():
+        print("Thanks for playing the Dice Rolling Game!")
+        break
+    else:
+        print("Please enter valid input (yes/no)")
+        continue
